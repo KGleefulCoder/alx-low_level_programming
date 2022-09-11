a4b3058341c26ac7a099c802536e29e52a6d3ba9
KGleefulCoder <khadijah6360@gmail.com>
2022-09-11 07:12:40 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
 /**
- * main - entry point for the program.
+ * main: entry point for the program.
  *
- * Description - A C Program taht prints with printf function
+ * Description: A C Program that prints with printf function
  *
- * Return - 0 if no errors, return non zero if errors.
+ * Return: 0 if no errors, return non zero if errors.
  */
 
 int main(void)
