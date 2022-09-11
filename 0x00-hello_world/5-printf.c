1e3d962d1d705c9cc3c835b0cbe16c274882d0c3
KGleefulCoder <khadijah6360@gmail.com>
2022-09-11 06:58:14 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,13 @@
 /**
  * main - entry point for the program.
  *
+ * Decription - A C Program taht prints with printf function
+ *
  * Return - 0 if no errors, return non zero if errors.
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of aret,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
