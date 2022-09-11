a18a94261c69ce9ab9c025f44da0d77d8d504221
KGleefulCoder <khadijah6360@gmail.com>
2022-09-11 06:55:14 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,9 @@
  *
  * Return - 0 if no errors, return non zero if errors.
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of aret,\n");
-	returm (0);
+	return (0);
 }
