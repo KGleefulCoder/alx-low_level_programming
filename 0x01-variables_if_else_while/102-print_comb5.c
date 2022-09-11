925a94092bb7e2130c046bf2a46ed8d20d4c61db
KGleefulCoder <khadijah6360@gmail.com>
2022-09-11 15:57:16 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,28 +11,27 @@ int main(void)
 {
 	int firstDigit = 0, seconDigit;
 
-	while (seconDigit <= 99)
+	while (firstDigit <= 99)
 	{
 		seconDigit = firstDigit;
 		while (seconDigit <= 99)
 		{
 			if (seconDigit != firstDigit)
 			{
-				Putchar((firstDigit / 10) + 48);
-				putchar((firstDigit % 10) + 48);      
+				putchar((firstDigit / 10) +48);
+				putchar((firstDigit % 10) + 48);
 				putchar(' ');
 				putchar((seconDigit / 10) + 48);
 				putchar((seconDigit % 10) + 48);
-
-			if (firstDigit != 98 || seconDigit != 99)
+				if (firstDigit != 98 || seconDigit != 99)
 				{
 					putchar(',');
-					putchar(' ');
+					putchar('');
 				}
 			}
-		++seconDigit;
+			++seconDigit;
 		}
-	++firstDigit;
+		++firstDigit;
 	}
 	putchar('\n');
 
