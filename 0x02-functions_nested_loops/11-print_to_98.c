e1eb387c59156ee73631560f482a1ef21fb6ff7e
KGleefulCoder <khadijah6360@gmail.com>
2022-09-14 17:00:12 -0700

    Task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,6 +12,7 @@ void print_to_98(int n)
 	int count;
 
 	if (n > 98)
+	{
 		for (count = n; count > 98; --count)
 		{
 			printf("%d", count);
@@ -22,4 +23,5 @@ void print_to_98(int n)
 			printf("%d", count);
 		}
 	printf("98\n");
+	}
 }
