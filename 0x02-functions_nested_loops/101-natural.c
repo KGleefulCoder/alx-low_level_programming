df96307451d02e0a1307d3c02b63ccf16a81dd38
KGleefulCoder <khadijah6360@gmail.com>
2022-09-16 09:30:31 -0700

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,4 +1,5 @@
 #include"main.h"
+#include<stdio.h>
 
 /**
  * main - Enty point
@@ -14,7 +15,7 @@ int main(void)
 
 	for (num = 0; num < 1024; ++num)
 	{
-		if((num % 3 == 0) || (num % 5 == 0))
+		if ((num % 3 == 0) || (num % 5 == 0))
 			sum += num;
 	}
 	printf("%d\n", sum);
