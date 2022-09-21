36a7127f175c15f5773f09971b9c5045e40a9b6c
KGleefulCoder <khadijah6360@gmail.com>
2022-09-21 11:47:51 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -18,9 +18,9 @@ char *_strcat(char *dest, char *src)
 		c++;
 
 	/* iterate through src array value without the null byte*/
-	for (c2 = 0; src[c2]; c2++)
+	for (c2 = 0; src[c2] ; c2++)
 		/*append src[c2] to dest[c] while overwriting the null byte in dest*/
-		dest[c++} = src[c2];
+		dest[c++] = src[c2];
 
 
 	return (dest);
